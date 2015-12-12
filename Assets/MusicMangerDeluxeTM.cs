0b58595cc070614e939d296c9218d625f48f3f79
Fredrik Melén <fredrik@moderskeppet.se>
2015-12-12 12:16:20 +0100

    drar ned djävel

---
+++ Assets/MusicMangerDeluxeTM.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicMangerDeluxeTM : MonoBehaviour {
+
+	public AudioSource sås;
+
+	// Use this for initialization
+	void Start () {
+		sås = gameObject.GetComponent<AudioSource> ();
+	}
+
+	void OnGUI () {
+		GUI.Label (new Rect (10, 10, 100, 20), "" + sås.timeSamples);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		sås.pitch += 0.003f;
+	}
+}
