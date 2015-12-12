72149ad826a2f23d1853b91cc9585db620f3f7e1
Christian Engvall <crilleengvall@gmail.com>
2015-12-12 11:39:38 +0100

    Adding simple movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public Vector2 Force;
+	private Rigidbody2D playerRigidbody;
+
+	void Start() {
+		this.playerRigidbody = this.GetComponent<Rigidbody2D>();
+	}
+
+	void Update () {
+		this.MovePlayer();
+	}
+
+	private void MovePlayer() {
+		if(Input.GetKeyDown(KeyCode.A)) {
+			this.playerRigidbody.AddForce(this.Force);
+		}
+	}
+}
